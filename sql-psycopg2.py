12e38b7be9d45b9a2506fcb891704d0d23e46be0
zanettiprado <>
2023-07-28 15:23:09 +0000

    create template

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -7,7 +7,22 @@ connection = psycopg2.connect(database="chinook")
 cursor = connection.cursor()
 
 # Query 1 - select  all records  from the "Artist" table
-cursor.execute('SELECT * FROM "Artist"')
+# cursor.execute('SELECT * FROM "Artist"')
+
+# Query 2 - select only the "Name" column from the "Artist" table
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# Query 3 - select only "Queen" from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Zeca Pagodinho"])
+
+# Query 4 - select only "ArtistId" #51 from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [155])
+
+# # Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [155])
+
+# Query 6 - select all tracks where the composer is "Queen" from the "Tracker" table
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Foo Fighters"])
 
 # fetch the results (multuple)
 results = cursor.fetchall()
